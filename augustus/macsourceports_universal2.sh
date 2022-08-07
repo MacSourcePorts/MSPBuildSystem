8986ad4eb2c0d0002a765dbb9b00889b8349dd5a
Tom Kidd <tomkidd@gmail.com>
2022-08-07 17:42:25 -0500

    Added Augustus/Julius

---
+++ augustus/macsourceports_universal2.sh
@@ -0,0 +1,53 @@
+# game/app specific values
+export APP_VERSION="3.1.0"
+export PRODUCT_NAME="augustus"
+export PROJECT_NAME="augustus"
+export PORT_NAME="Augustus"
+export ICONSFILENAME="augustus"
+export EXECUTABLE_NAME="augustus"
+export GIT_TAG="v3.1.0"
+export GIT_DEFAULT_BRANCH="master"
+
+#constants
+source ../common/constants.sh
+
+cd ../../${PROJECT_NAME}
+
+# reset to the main branch
+echo git checkout ${GIT_DEFAULT_BRANCH}
+git checkout ${GIT_DEFAULT_BRANCH}
+
+# fetch the latest 
+echo git pull
+git pull
+
+# check out the latest release tag
+echo git checkout tags/${GIT_TAG}
+git checkout tags/${GIT_TAG}
+
+rm -rf ${BUILT_PRODUCTS_DIR}
+
+# create makefiles with cmake, perform builds with make
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+cd ${X86_64_BUILD_FOLDER}
+/usr/local/bin/cmake ..
+make -j$NCPU
+
+cd ..
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
+cd ${ARM64_BUILD_FOLDER}
+cmake ..
+make -j$NCPU
+
+cd ..
+
+# create the app bundle
+"../MSPBuildSystem/common/build_app_bundle.sh"
+
+#sign and notarize
+"../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
+
+#create dmg
+"../MSPBuildSystem/common/package_dmg.sh"
