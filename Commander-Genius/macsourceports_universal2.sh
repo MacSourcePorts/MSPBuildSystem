c1ae1fa809f09fb43ef90a77e07ddd68dfa688a2
Tom Kidd <tomkidd@gmail.com>
2022-09-09 16:57:52 -0500

    Added Commander-Genius

---
+++ Commander-Genius/macsourceports_universal2.sh
@@ -0,0 +1,66 @@
+# game/app specific values
+export APP_VERSION="3.1.3"
+export PRODUCT_NAME="CGenius"
+export PROJECT_NAME="Commander-Genius"
+export PORT_NAME="Commander Genius"
+export ICONSFILENAME="cglogo"
+export EXECUTABLE_NAME="CGeniusExe"
+export PKGINFO="APPLCKEN"
+export GIT_TAG="3.1.3"
+export GIT_DEFAULT_BRANCH="master"
+
+#constants
+source ../common/constants.sh
+
+cd ../../${PROJECT_NAME}
+
+# reset to the main branch
+echo git checkout ${GIT_DEFAULT_BRANCH}
+git checkout ${GIT_DEFAULT_BRANCH}
+
+# # fetch the latest 
+echo git pull
+git pull
+
+rm -rf ${BUILT_PRODUCTS_DIR}
+
+# create makefiles with cmake, perform builds with make
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+mkdir -p ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
+cd ${X86_64_BUILD_FOLDER}
+cmake \
+-DMACOS_APP_BUNDLE=ON \
+-DCMAKE_OSX_ARCHITECTURES=x86_64 \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/usr/local \
+-DCMAKE_INSTALL_PREFIX=/usr/local \
+..
+make -j$NCPU
+cp src/${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+
+cd ..
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
+mkdir -p ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
+cd ${ARM64_BUILD_FOLDER}
+cmake  \
+-DMACOS_APP_BUNDLE=ON \
+-DCMAKE_OSX_ARCHITECTURES=arm64 \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/opt/Homebrew \
+-DCMAKE_INSTALL_PREFIX=/opt/Homebrew \
+..
+make -j$NCPU
+cp src/${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+
+cd ..
+
+# create the app bundle
+"../MSPBuildSystem/common/build_app_bundle.sh"
+
+# sign and notarize
+"../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
+
+# create dmg
+"../MSPBuildSystem/common/package_dmg.sh"
