d294a2f7f343e85041af649f265190e6b99971bc
Tom Kidd <tomkidd@gmail.com>
2022-10-10 17:55:09 -0500

    Added RigelEngine

---
+++ RigelEngine/macsourceports_universal2.sh
@@ -0,0 +1,67 @@
+# game/app specific values
+export APP_VERSION="0.8.5"
+export PRODUCT_NAME="RigelEngine"
+export PROJECT_NAME="RigelEngine"
+export PORT_NAME="RigelEngine"
+export ICONSFILENAME="RigelEngine"
+export EXECUTABLE_NAME="RigelEngine"
+export PKGINFO="APPLROTT"
+export GIT_DEFAULT_BRANCH="master"
+export GIT_TAG="v0.8.5"
+
+#constants
+source ../common/constants.sh
+
+cd ../../${PROJECT_NAME}
+
+# reset to the main branch
+echo git checkout ${GIT_DEFAULT_BRANCH}
+git checkout ${GIT_DEFAULT_BRANCH}
+
+# fetch the latest 
+echo git pull
+git pull
+
+# check out the latest release tag
+echo git checkout tags/${GIT_TAG}
+git checkout tags/${GIT_TAG}
+
+
+rm -rf ${BUILT_PRODUCTS_DIR}
+
+# create makefiles with cmake, perform builds with make
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+cd ${X86_64_BUILD_FOLDER}
+cmake \
+-DCMAKE_OSX_ARCHITECTURES=x86_64 \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/usr/local \
+-DCMAKE_INSTALL_PREFIX=/usr/local \
+..
+make -j$NCPU
+mv src/${WRAPPER_NAME} .
+
+cd ..
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
+cd ${ARM64_BUILD_FOLDER}
+cmake  \
+-DCMAKE_OSX_ARCHITECTURES=arm64 \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/opt/Homebrew \
+-DCMAKE_INSTALL_PREFIX=/opt/Homebrew \
+..
+make -j$NCPU
+mv src/${WRAPPER_NAME} .
+
+cd ..
+
+# create the app bundle
+"../MSPBuildSystem/common/build_app_bundle.sh"
+
+# #sign and notarize
+"../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
+
+# #create dmg
+"../MSPBuildSystem/common/package_dmg.sh"
