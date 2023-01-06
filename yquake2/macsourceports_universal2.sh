3d1f5be9252d7917b1010e2f654ab9adf5def643
Tom Kidd <tomkidd@gmail.com>
2023-01-05 20:51:47 -0600

    Added mission packs to yquake2

--- yquake2/macsourceports_universal2.sh
+++ yquake2/macsourceports_universal2.sh
@@ -8,12 +8,73 @@ export EXECUTABLE_NAME="quake2"
 export PKGINFO="APPLGYQ2"
 export GIT_TAG="QUAKE2_8_20"
 export GIT_DEFAULT_BRANCH="master"
+export GIT_TAG_XATRIX="XATRIX_2_11"
+export GIT_TAG_ROGUE="ROGUE_2_10"
 
 # constants
 source ../common/constants.sh
 
-cd ../../${PROJECT_NAME}
+ARM64_CFLAGS="-I/opt/homebrew/include -I/opt/homebrew/opt/openal-soft/include -mmacosx-version-min=10.9"
+ARM64_LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/openal-soft/lib -mmacosx-version-min=10.9"
+x86_64_CFLAGS="-mmacosx-version-min=10.9"
+x86_64_LDFLAGS="-mmacosx-version-min=10.9"
 
+# build the expansion pack libraries first
+
+# Mission Pack 1: The Reckoning (XATRIX)
+cd ../../xatrix
+# reset to the main branch
+echo git checkout ${GIT_DEFAULT_BRANCH}
+git checkout ${GIT_DEFAULT_BRANCH}
+
+# fetch the latest 
+echo git pull
+git pull
+
+# check out the latest release tag
+echo git checkout tags/${GIT_TAG_XATRIX}
+git checkout tags/${GIT_TAG_XATRIX}
+
+(YQ2_ARCH=x86_64 make clean) || exit 1;
+(YQ2_ARCH=x86_64 CFLAGS=$x86_64_CFLAGS  LDFLAGS=$x86_64_LDFLAGS make -j$NCPU) || exit 1;
+mkdir -p ${X86_64_BUILD_FOLDER}
+mv release/* ${X86_64_BUILD_FOLDER}
+rm -rd release
+
+(YQ2_ARCH=arm64 make clean) || exit 1;
+(YQ2_ARCH=arm64 CFLAGS=$ARM64_CFLAGS  LDFLAGS=$ARM64_LDFLAGS make -j$NCPU) || exit 1;
+mkdir -p ${ARM64_BUILD_FOLDER}
+mv release/* ${ARM64_BUILD_FOLDER}
+rm -rd release
+
+# Mission Pack 2: Ground Zero (ROGUE)
+cd ../rogue
+# reset to the main branch
+echo git checkout ${GIT_DEFAULT_BRANCH}
+git checkout ${GIT_DEFAULT_BRANCH}
+
+# fetch the latest 
+echo git pull
+git pull
+
+# check out the latest release tag
+echo git checkout tags/${GIT_TAG_ROGUE}
+git checkout tags/${GIT_TAG_ROGUE}
+
+(YQ2_ARCH=x86_64 make clean) || exit 1;
+(YQ2_ARCH=x86_64 CFLAGS=$x86_64_CFLAGS  LDFLAGS=$x86_64_LDFLAGS make -j$NCPU) || exit 1;
+mkdir -p ${X86_64_BUILD_FOLDER}
+mv release/* ${X86_64_BUILD_FOLDER}
+rm -rd release
+
+(YQ2_ARCH=arm64 make clean) || exit 1;
+(YQ2_ARCH=arm64 CFLAGS=$ARM64_CFLAGS  LDFLAGS=$ARM64_LDFLAGS make -j$NCPU) || exit 1;
+mkdir -p ${ARM64_BUILD_FOLDER}
+mv release/* ${ARM64_BUILD_FOLDER}
+rm -rd release
+
+# Main game compilation
+cd ../${PROJECT_NAME}
 # reset to the main branch
 echo git checkout ${GIT_DEFAULT_BRANCH}
 git checkout ${GIT_DEFAULT_BRANCH}
@@ -29,11 +90,6 @@ git checkout tags/${GIT_TAG}
 rm -rf ${BUILT_PRODUCTS_DIR}
 rm -rf ${X86_64_BUILD_FOLDER}
 rm -rf ${ARM64_BUILD_FOLDER}
-
-ARM64_CFLAGS="-I/opt/homebrew/include -I/opt/homebrew/opt/openal-soft/include -mmacosx-version-min=10.9"
-ARM64_LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/openal-soft/lib -mmacosx-version-min=10.9"
-x86_64_CFLAGS="-mmacosx-version-min=10.9"
-x86_64_LDFLAGS="-mmacosx-version-min=10.9"
 
 (YQ2_ARCH=x86_64 make clean) || exit 1;
 (YQ2_ARCH=x86_64 CFLAGS=$x86_64_CFLAGS  LDFLAGS=$x86_64_LDFLAGS make -j$NCPU) || exit 1;
@@ -55,6 +111,14 @@ if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/baseq2" ]; then
 	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/baseq2" || exit 1;
 fi
 
+if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/xatrix" ]; then
+	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/xatrix" || exit 1;
+fi
+
+if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/rogue" ]; then
+	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/rogue" || exit 1;
+fi
+
 #dylibbundler the quake2 libs
 dylibbundler -od -b -x ./${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_gl1.dylib -d ./${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/${X86_64_LIBS_FOLDER}/ -p @executable_path/${X86_64_LIBS_FOLDER}/
 dylibbundler -od -b -x ./${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_gl3.dylib -d ./${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/${X86_64_LIBS_FOLDER}/ -p @executable_path/${X86_64_LIBS_FOLDER}/
@@ -70,6 +134,8 @@ lipo ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_gl1.dylib ${ARM64_BUIL
 lipo ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_gl3.dylib ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_gl3.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/ref_gl3.dylib" -create
 lipo ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_soft.dylib ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/ref_soft.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/ref_soft.dylib" -create
 lipo ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/baseq2/game.dylib ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}/baseq2/game.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/baseq2/game.dylib" -create
+lipo ../xatrix/${X86_64_BUILD_FOLDER}/game.dylib ../xatrix/${ARM64_BUILD_FOLDER}/game.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/xatrix/game.dylib" -create
+lipo ../rogue/${X86_64_BUILD_FOLDER}/game.dylib ../rogue/${ARM64_BUILD_FOLDER}/game.dylib -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/rogue/game.dylib" -create
 
 #sign and notarize
 "../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
