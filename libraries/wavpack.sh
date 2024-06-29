eb3175a9e42c18f965b282a1e12a56dfca63eff5
Tom Kidd <tomkidd@gmail.com>
2024-06-29 13:28:21 -0500

    Added more libs

---
+++ libraries/wavpack.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://www.wavpack.com/wavpack-5.7.0.tar.bz2"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+"../common/cmake_build.sh"
---
+++ libraries/xmp.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://downloads.sourceforge.net/project/xmp/libxmp/4.6.0/libxmp-4.6.0.tar.gz"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+"../common/cmake_build.sh"
