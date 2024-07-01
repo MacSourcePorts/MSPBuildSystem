961821cd94b560acf912846d9a74983452f944c9
Tom Kidd <tomkidd@gmail.com>
2024-07-01 17:36:14 -0500

    Added game-music-emu, expanded SOURCE_FILE
    Now we can pass in a SOURCE_FILE if the downloaded zip file is different from the one in the URL

---
+++ libraries/game-music-emu.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/libgme/game-music-emu/archive/refs/tags/0.6.3.zip"
+export SOURCE_FILE="game-music-emu-0.6.3.zip"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON -DINSTALL_MANPAGES=OFF"
+
+"../common/cmake_build.sh"
