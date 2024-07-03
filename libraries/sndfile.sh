a8c7ad7fb4f385e5d1bb0ae155b27b9f2584cada
Tom Kidd <tomkidd@gmail.com>
2024-07-03 14:55:13 -0500

    Added libsndfile

---
+++ libraries/sndfile.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://github.com/libsndfile/libsndfile/releases/download/1.2.2/libsndfile-1.2.2.tar.xz"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON -DINSTALL_MANPAGES=OFF"
+
+"../common/cmake_build.sh"
