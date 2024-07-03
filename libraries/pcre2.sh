d150edea20b8df82dc7ec40bf3840b1dc1747839
Tom Kidd <tomkidd@gmail.com>
2024-07-03 15:08:27 -0500

    Added pcre2

---
+++ libraries/pcre2.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.bz2"
+export MAKE_ARGS="--disable-static --enable-shared --disable-dependency-tracking --enable-pcre2-16 --enable-pcre2-32 --enable-pcre2grep-libz --enable-pcre2grep-libbz2 --enable-jit --enable-pcre2test-libedit"
+
+"../common/make_build.sh"
