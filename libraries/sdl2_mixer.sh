d64fa50d7f2649ee0abce914fb69ffd3ee0ef815
Tom Kidd <tomkidd@gmail.com>
2024-07-05 11:58:41 -0500

    SDL2_mixer now works

--- libraries/sdl2_mixer.sh
+++ libraries/sdl2_mixer.sh
@@ -2,21 +2,3 @@ export SOURCE_URL="https://github.com/libsdl-org/SDL_mixer/releases/download/rel
 export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
 
 "../common/cmake_build.sh"
-
-# rm -rf source
-# mkdir source
-# cd source
-# curl -JLO https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.8.0/SDL2_mixer-2.8.0.zip
-# unzip SDL2_mixer-2.8.0.zip
-# cd SDL2_mixer-2.8.0
-
-# export MACOSX_DEPLOYMENT_TARGET="10.7"
-
-# cd ../..
-# rm -rf build
-# mkdir build
-# cd build
-
-# cmake ../source/SDL2_mixer-2.8.0 "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
-# cmake --build .
-# cmake --install .
