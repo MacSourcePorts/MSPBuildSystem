2ccca7d63c1d511c9d26e11ad039b22d5ceccbd1
Tom Kidd <tomkidd@gmail.com>
2024-07-05 15:13:27 -0500

    Tweaked SDL2_Net, converted Abuse and Tyrian 2000

--- Abuse_1996/macsourceports_universal2.sh
+++ Abuse_1996/macsourceports_universal2.sh
@@ -15,40 +15,64 @@ cd ../../${PROJECT_NAME}
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 
-# create makefiles with cmake, perform builds with make
-rm -rf ${X86_64_BUILD_FOLDER}
-mkdir ${X86_64_BUILD_FOLDER}
-cd ${X86_64_BUILD_FOLDER}
-cmake \
--DMACOS_APP_BUNDLE=ON \
--DCMAKE_OSX_ARCHITECTURES=x86_64 \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
--DCMAKE_PREFIX_PATH=/usr/local \
--DCMAKE_INSTALL_PREFIX=/usr/local \
-..
-make -j$NCPU
-mv src/${WRAPPER_NAME} .
-mkdir -p ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
-cp -a ../data/* ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+    mkdir ${BUILT_PRODUCTS_DIR}
+    cd ${BUILT_PRODUCTS_DIR}
+    cmake \
+    -DMACOS_APP_BUNDLE=ON \
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    -DCMAKE_PREFIX_PATH=/usr/local \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    ..
+    # make -j$NCPU
+    cmake --build . --parallel $NCPU
+    mv src/${WRAPPER_NAME} .
+    install_name_tool -add_rpath @executable_path/. ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+    cp /usr/local/lib/libSDL2-2.0.0.dylib ${EXECUTABLE_FOLDER_PATH}
+    cp /usr/local/lib/libSDL2_mixer-2.0.801.0.0.dylib ${EXECUTABLE_FOLDER_PATH}
+    mkdir -p ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
+    cp -a ../data/* ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
+else
+    # create makefiles with cmake, perform builds with make
+    rm -rf ${X86_64_BUILD_FOLDER}
+    mkdir ${X86_64_BUILD_FOLDER}
+    cd ${X86_64_BUILD_FOLDER}
+    cmake \
+    -DMACOS_APP_BUNDLE=ON \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DCMAKE_PREFIX_PATH=/usr/local \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    ..
+    make -j$NCPU
+    mv src/${WRAPPER_NAME} .
+    mkdir -p ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
+    cp -a ../data/* ${UNLOCALIZED_RESOURCES_FOLDER_PATH}/data
 
-cd ..
-rm -rf ${ARM64_BUILD_FOLDER}
-mkdir ${ARM64_BUILD_FOLDER}
-cd ${ARM64_BUILD_FOLDER}
-cmake  \
--DMACOS_APP_BUNDLE=ON \
--DCMAKE_OSX_ARCHITECTURES=arm64 \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
--DCMAKE_PREFIX_PATH=/opt/Homebrew \
--DCMAKE_INSTALL_PREFIX=/opt/Homebrew \
-..
-make -j$NCPU
-mv src/${WRAPPER_NAME} .
+    cd ..
+    rm -rf ${ARM64_BUILD_FOLDER}
+    mkdir ${ARM64_BUILD_FOLDER}
+    cd ${ARM64_BUILD_FOLDER}
+    cmake  \
+    -DMACOS_APP_BUNDLE=ON \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DCMAKE_PREFIX_PATH=/opt/Homebrew \
+    -DCMAKE_INSTALL_PREFIX=/opt/Homebrew \
+    ..
+    make -j$NCPU
+    mv src/${WRAPPER_NAME} .
+fi
 
 cd ..
 
 # create the app bundle
-"../MSPBuildSystem/common/build_app_bundle.sh"
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+    "../MSPBuildSystem/common/build_app_bundle.sh" "skiplipo"
+else
+    "../MSPBuildSystem/common/build_app_bundle.sh"
+fi
 
 # sign and notarize
 "../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
--- libraries/sdl2_net.sh
+++ libraries/sdl2_net.sh
@@ -1,4 +1,6 @@
 export SOURCE_URL="https://github.com/libsdl-org/SDL_net/releases/download/release-2.2.0/SDL2_net-2.2.0.zip"
-export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
+export MAKE_ARGS="--disable-dependency-tracking --disable-sdltest"
 
-"../common/cmake_build.sh"
+"../common/make_build.sh"
+
+sudo install_name_tool -id "@rpath/libSDL2_net-2.0.0.dylib" /usr/local/lib/libSDL2_net-2.0.0.dylib
