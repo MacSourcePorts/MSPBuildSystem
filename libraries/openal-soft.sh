2752a1826922816af2538cead05695bee5e4c10d
Tom Kidd <tomkidd@gmail.com>
2024-07-06 15:49:45 -0500

    Added openal-soft

---
+++ libraries/openal-soft.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://openal-soft.org/openal-releases/openal-soft-1.23.1.tar.bz2"
+export CMAKE_ARGS="-DALSOFT_BACKEND_PORTAUDIO=OFF -DALSOFT_BACKEND_PULSEAUDIO=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_MIDI_FLUIDSYNTH=OFF"
+
+"../common/cmake_build.sh"
