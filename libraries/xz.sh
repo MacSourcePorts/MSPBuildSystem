9afb30772f93f8f0330009f282d89a168f5836e8
Tom Kidd <tomkidd@gmail.com>
2024-07-15 10:58:11 -0500

    Added xz

---
+++ libraries/xz.sh
@@ -0,0 +1,4 @@
+export SOURCE_URL="https://github.com/tukaani-project/xz/releases/download/v5.4.6/xz-5.4.6.tar.gz"
+export MAKE_ARGS="--disable-silent-rules --disable-nls --enable-static"
+
+"../common/make_build.sh"
