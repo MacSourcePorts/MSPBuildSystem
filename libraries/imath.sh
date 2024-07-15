6ec00ab27893fa2bfb6e584c964d0a431617dc36
Tom Kidd <tomkidd@gmail.com>
2024-07-15 16:13:14 -0500

    Added imath

---
+++ libraries/imath.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.11.tar.gz"
+export SOURCE_FILE="Imath-3.1.11.tar.gz"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+"../common/cmake_build.sh"
