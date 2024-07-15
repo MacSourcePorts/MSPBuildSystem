203bcef6344feb68fbf327c3cdba08e728bce020
Tom Kidd <tomkidd@gmail.com>
2024-07-15 14:09:51 -0500

    Added lz4 and simpler make script
    lz4 is the first one I've run into without "configure"

---
+++ libraries/lz4.sh
@@ -0,0 +1,5 @@
+# https://github.com/lz4/lz4/archive/refs/tags/v1.9.4.tar.gz
+export SOURCE_URL="https://github.com/lz4/lz4/releases/download/v1.9.4/lz4-1.9.4.tar.gz"
+export MAKE_ARGS=""
+
+"../common/make_build2.sh"
