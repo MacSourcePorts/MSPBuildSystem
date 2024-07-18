2b6cc52dc2f4f4993123d95dd99fc0d3fea4c0f7
Tom Kidd <tomkidd@gmail.com>
2024-07-17 21:22:04 -0500

    Added brotli

---
+++ libraries/brotli.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/google/brotli/archive/refs/tags/v1.1.0.tar.gz"
+export SOURCE_FILE="brotli-1.1.0.tar.gz"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON"
+
+"../common/cmake_build.sh"
