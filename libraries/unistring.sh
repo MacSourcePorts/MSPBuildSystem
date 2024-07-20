1858ce54b3464be5b03d21dcf3c3fc143319e6f7
Tom Kidd <tomkidd@gmail.com>
2024-07-20 17:42:07 -0500

    Added unistring

---
+++ libraries/unistring.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
