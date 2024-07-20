70432c9e5463a7d99ed4f3a65479390439dc6241
Tom Kidd <tomkidd@gmail.com>
2024-07-20 17:29:39 -0500

    Added idn2

--- libraries/fontconfig.sh
+++ libraries/fontconfig.sh
@@ -1,4 +1,3 @@
-# https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.15.0.tar.xz
 export SOURCE_URL="https://downloads.sourceforge.net/project/freetype/freetype2/2.13.2/freetype-2.13.2.tar.xz"
 export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules --disable-docs --enable-static--with-add-fonts=/System/Library/Fonts,/Library/Fonts,~/Library/Fonts"
 
