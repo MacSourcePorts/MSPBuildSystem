705e485a907518bbefa10d1c4bde6418ef17f481
Tom Kidd <tomkidd@gmail.com>
2024-07-22 20:13:36 -0500

    Added expat

---
+++ libraries/expat.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/libexpat/libexpat/releases/download/R_2_6_2/expat-2.6.2.tar.lz"
+export MAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
