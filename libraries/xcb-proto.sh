9043fc8dd97a4aaa8ca5029b7d69293bb5cbf6c2
Tom Kidd <tomkidd@gmail.com>
2024-07-22 21:42:16 -0500

    Added xcb-proto

---
+++ libraries/xcb-proto.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.17.0.tar.xz"
+export MAKE_ARGS="--disable-silent-rules"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
