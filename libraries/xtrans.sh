a4e5bb0b0e7cfa37d4a26d700fe244164e5dc395
Tom Kidd <tomkidd@gmail.com>
2024-07-22 20:29:38 -0500

    Added xtrans

---
+++ libraries/xtrans.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://www.x.org/archive/individual/lib/xtrans-1.5.0.tar.xz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules --enable-docs=no"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
