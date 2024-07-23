ad3f9a5b88595c9304c7e41565bb80916231e8a9
Tom Kidd <tomkidd@gmail.com>
2024-07-22 21:19:40 -0500

    Added xorgproto

---
+++ libraries/xorgproto.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2024.1.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
