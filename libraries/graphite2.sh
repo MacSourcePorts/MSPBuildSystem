8c314401e406921ab65a82899257e1db0a10e095
Tom Kidd <tomkidd@gmail.com>
2024-07-23 17:04:00 -0500

    Added graphite2

---
+++ libraries/graphite2.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz"
+export SOURCE_FOLDER="graphite2-1.3.14"
+export CMAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/cmake_build.sh"
