e90cd644649e8254d551a01c6e32f133290a1859
Tom Kidd <tomkidd@gmail.com>
2024-07-22 21:44:33 -0500

    Added pixman

---
+++ libraries/pixman.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://cairographics.org/releases/pixman-0.42.2.tar.gz"
+export MAKE_ARGS="--disable-silent-rules --disable-gtk"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
