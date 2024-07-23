cb26e79c40ba7d62c58196ba46b6e8938c2dd058
Tom Kidd <tomkidd@gmail.com>
2024-07-22 20:49:03 -0500

    Added lzo

---
+++ libraries/lzo.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --enable-shared"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
+make check
