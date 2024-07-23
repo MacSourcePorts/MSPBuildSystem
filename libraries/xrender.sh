46462e2e89ff74247c300ea1350f56db65c11a57
Tom Kidd <tomkidd@gmail.com>
2024-07-22 21:54:07 -0500

    Added xrender

---
+++ libraries/xrender.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://www.x.org/archive/individual/lib/libXrender-0.9.11.tar.gz"
+export MAKE_ARGS="--disable-silent-rules --disable-silent-rules"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
