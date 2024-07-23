05f5a005ad4ff832a962bb057adc5df48c4c0547
Tom Kidd <tomkidd@gmail.com>
2024-07-22 21:30:47 -0500

    Add xdmcp

---
+++ libraries/xdmcp.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://www.x.org/archive/individual/lib/libXdmcp-1.1.5.tar.xz"
+export MAKE_ARGS="--disable-silent-rules --disable-silent-rules --enable-docs=no"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
