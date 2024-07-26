c8d08ae74933d03a00991e79752ea4344b8603d4
Tom Kidd <tomkidd@gmail.com>
2024-07-26 16:01:04 -0500

    Added unibreak

---
+++ libraries/unibreak.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/adah1972/libunibreak/releases/download/libunibreak_6_1/libunibreak-6.1.tar.gz"
+export MAKE_ARGS="--disable-silent-rules"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
