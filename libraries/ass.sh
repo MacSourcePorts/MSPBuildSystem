4b4601bc31448ed9b341d4b811c43647ebf31b1c
Tom Kidd <tomkidd@gmail.com>
2024-07-27 20:27:56 -0500

    Modified libass to use make lipo script
    Since it uses Assembly

--- libraries/ass.sh
+++ libraries/ass.sh
@@ -2,4 +2,4 @@ export SOURCE_URL="https://github.com/libass/libass/releases/download/0.17.3/lib
 export MAKE_ARGS="--disable-dependency-tracking"
 
 source "../common/get_source.sh"
-source "../common/make_build.sh"
+source "../common/make_build_lipo.sh"
