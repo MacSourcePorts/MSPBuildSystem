30a477ab2fd83935f09d938ffb4cfea9599ae0da
Tom Kidd <tomkidd@gmail.com>
2024-07-29 15:16:19 -0500

    Added ssh2

---
+++ libraries/ssh2.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://libssh2.org/download/libssh2-1.11.0.tar.gz"
+export MAKE_ARGS="--disable-silent-rules --disable-examples-build --with-openssl --with-libz"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
