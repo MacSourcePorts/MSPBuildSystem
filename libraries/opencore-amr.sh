768513ac9a55ea3e715dc4e947dec1a5277fa77f
Tom Kidd <tomkidd@gmail.com>
2024-07-29 15:01:32 -0500

    Added opencore-amr

---
+++ libraries/opencore-amr.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://downloads.sourceforge.net/project/opencore-amr/opencore-amr/opencore-amr-0.1.6.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
