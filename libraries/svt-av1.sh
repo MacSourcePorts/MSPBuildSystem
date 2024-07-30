06a4932caa6b165475d67e75d080bf1284de12e2
Tom Kidd <tomkidd@gmail.com>
2024-07-29 20:18:01 -0500

    Added svt-av1

---
+++ libraries/svt-av1.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v2.1.2/SVT-AV1-v2.1.2.tar.bz2"
+export CMAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/cmake_build.sh"
