35a74cd84fc0ef6287c5e947c5005c916028595c
Tom Kidd <tomkidd@gmail.com>
2024-07-29 20:33:22 -0500

    Added b2

---
+++ libraries/b2.sh
@@ -0,0 +1,10 @@
+export SOURCE_URL="https://github.com/BLAKE2/libb2/releases/download/v0.98.1/libb2-0.98.1.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rule"
+
+source "../common/get_source.sh"
+
+curl -JLO https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff
+mv configure-big_sur.diff source
+patch -d source/${SOURCE_FOLDER} < source/configure-big_sur.diff
+
+source "../common/make_build.sh"
