93207c44146f5c433326723568dd399ef2cd08b0
Tom Kidd <tomkidd@gmail.com>
2024-07-29 21:17:20 -0500

    Added x265

---
+++ libraries/x265.sh
@@ -0,0 +1,7 @@
+export SOURCE_URL="https://bitbucket.org/multicoreware/x265_git/get/3.6.tar.gz"
+export SOURCE_FILE="multicoreware-x265_git-aa7f602f7592.tar.gz"
+export SOURCE_FOLDER="multicoreware-x265_git-aa7f602f7592/source"
+export CMAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/cmake_build.sh"
