86f2811cbde3e127ad747d941ea57c13540a2ac1
Tom Kidd <tomkidd@gmail.com>
2024-07-30 17:20:51 -0500

    Added zimg
    Of course the last one is a pain. This one wouldn't work unless I also set CXXFLAGS as well. First one in ages that needed it.

--- common/make_build_lipo.sh
+++ common/make_build_lipo.sh
@@ -9,13 +9,13 @@ mkdir build-arm64 build-x86_64 build-universal2
 
 # Build for arm64
 cd build-arm64
-../configure ${MAKE_ARGS} CFLAGS="-arch arm64" LDFLAGS="-arch arm64" --host=aarch64-apple-darwin
+../configure ${MAKE_ARGS} CFLAGS="-arch arm64" CXXFLAGS="-arch arm64" LDFLAGS="-arch arm64" --host=aarch64-apple-darwin
 make  #-j$NCPU
 cd ..
 
 # Build for x86_64
 cd build-x86_64
-../configure ${MAKE_ARGS} CFLAGS="-arch x86_64" LDFLAGS="-arch x86_64" --host=x86_64-apple-darwin
+../configure ${MAKE_ARGS} CFLAGS="-arch x86_64" CXXFLAGS="-arch x86_64" LDFLAGS="-arch x86_64" --host=x86_64-apple-darwin
 make  #-j$NCPU
 cd ..
 
---
+++ libraries/zimg.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/sekrit-twc/zimg/archive/refs/tags/release-3.0.5.tar.gz"
+export SOURCE_FILE="zimg-release-3.0.5.tar.gz"
+
+source "../common/get_source.sh"
+source "../common/make_build_lipo.sh"
