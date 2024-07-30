10092eb108445136a9a0948eb93d33ce54a64c47
Tom Kidd <tomkidd@gmail.com>
2024-07-29 20:33:13 -0500

    Added liptonica

---
+++ libraries/leptonica.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/DanBloomberg/leptonica/releases/download/1.84.1/leptonica-1.84.1.tar.gz"
+export MAKE_ARGS="--with-libwebp --with-libopenjpeg"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
