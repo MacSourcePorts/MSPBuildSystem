d70c0fca00cbfc1df9266bb5e439b24fb90cd76a
Tom Kidd <tomkidd@gmail.com>
2024-08-01 17:20:27 -0500

    Added glew

---
+++ libraries/glew.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://downloads.sourceforge.net/project/glew/glew/2.2.0/glew-2.2.0.tgz"
+export SOURCE_FOLDER="glew-2.2.0/build/cmake"
+export CMAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/cmake_build.sh"
