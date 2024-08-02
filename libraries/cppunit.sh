0a9a7191e5b5a0ee5a63dabeb220532fcd77cfd5
Tom Kidd <tomkidd@gmail.com>
2024-08-02 16:22:11 -0500

    Added cppunit

---
+++ libraries/cppunit.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://dev-www.libreoffice.org/src/cppunit-1.15.1.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
