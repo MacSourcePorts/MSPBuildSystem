6f08c531e96920291a3be47b78dfdbf2d0d47c2d
Tom Kidd <tomkidd@gmail.com>
2024-08-02 17:06:18 -0500

    Added freealut

---
+++ libraries/freealut.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://deb.debian.org/debian/pool/main/f/freealut/freealut_1.1.0.orig.tar.gz"
+export SOURCE_FOLDER="freealut-1.1.0"
+export MAKE_ARGS="--disable-debug --disable-dependency-tracking"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
