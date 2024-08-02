e8d2fc0a872d3dd89bd20d071e54f084a4f3e69c
Tom Kidd <tomkidd@gmail.com>
2024-08-02 16:17:49 -0500

    Added texinfo

---
+++ libraries/texinfo.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://ftp.gnu.org/gnu/texinfo/texinfo-7.1.tar.xz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-install-warnings"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
