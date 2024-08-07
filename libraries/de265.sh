429913e12d210f1ccd9f7f924a6656805aeac845
Tom Kidd <tomkidd@gmail.com>
2024-08-07 16:28:58 -0500

    Added de265

---
+++ libraries/de265.sh
@@ -0,0 +1,10 @@
+export SOURCE_URL="https://github.com/strukturag/libde265/releases/download/v1.0.15/libde265-1.0.15.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules --disable-sherlock265 --disable-dec265"
+
+source "../common/get_source.sh"
+
+curl -JLO https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff
+mv configure-big_sur.diff source
+patch -d source/${SOURCE_FOLDER} < source/configure-big_sur.diff
+
+source "../common/make_build.sh"
