09d68506353ce33dc1400ff061f95d7703430949
Tom Kidd <tomkidd@gmail.com>
2024-08-13 19:53:57 -0500

    Added jbog2dec

---
+++ libraries/jbig2dec.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://github.com/ArtifexSoftware/jbig2dec/archive/refs/tags/0.20.tar.gz"
+export SOURCE_FILE="jbig2dec-0.20.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-silent-rules --without-libpng"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
