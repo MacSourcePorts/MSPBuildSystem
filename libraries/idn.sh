6f023fb31ae71a217fab72a8cefc9e827876595a
Tom Kidd <tomkidd@gmail.com>
2024-08-13 19:53:41 -0500

    Added idn

---
+++ libraries/idn.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://ftp.gnu.org/gnu/libidn/libidn-1.42.tar.gz"
+export MAKE_ARGS="--disable-dependency-tracking --disable-csharp"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
