927f035107d4cc8437c8e412a6f6ae69f1f57d9c
Tom Kidd <tomkidd@gmail.com>
2024-08-13 20:02:23 -0500

    Added epoxy

---
+++ libraries/epoxy.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://download.gnome.org/sources/libepoxy/1.5/libepoxy-1.5.10.tar.xz"
+export MESON_FLAGS=""
+
+source "../common/get_source.sh"
+source "../common/meson_build.sh"
