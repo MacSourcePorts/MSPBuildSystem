8f8762eaaf39f7ed53f0477fa1c29b71d0223892
Tom Kidd <tomkidd@gmail.com>
2024-08-13 21:58:38 -0500

    Added speexdsp

---
+++ libraries/speexdsp.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://github.com/xiph/speexdsp/archive/refs/tags/SpeexDSP-1.2.1.tar.gz"
+export SOURCE_FILE="speexdsp-SpeexDSP-1.2.1.tar.gz"
+export MAKE_ARGS="--disable-debug --disable-dependency-tracking"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
