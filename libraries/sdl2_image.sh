0922258f9cf2efd98f3b788a1b32e012a10aea11
Tom Kidd <tomkidd@gmail.com>
2024-08-13 21:50:27 -0500

    Added sdl2_image

---
+++ libraries/sdl2_image.sh
@@ -0,0 +1,5 @@
+export SOURCE_URL="https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.2/SDL2_image-2.8.2.tar.gz"
+export MAKE_ARGS="--disable-imageio --disable-avif-shared --disable-jpg-shared --disable-jxl-shared --disable-png-shared --disable-stb-image --disable-tif-shared --disable-webp-shared"
+
+source "../common/get_source.sh"
+source "../common/make_build.sh"
