eb93573fdc97d8be38e1a36f4e5321a03a550ce7
Tom Kidd <tomkidd@gmail.com>
2024-10-09 12:18:07 -0500

    Fix to make giflib universal2

--- libraries/giflib.sh
+++ libraries/giflib.sh
@@ -2,5 +2,5 @@ export SOURCE_URL="https://downloads.sourceforge.net/project/giflib/giflib-5.2.2
 export CONFIGURE_ARGS=""
 
 source "../common/get_source.sh"
-gsed -i 's/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS)/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS) -arch arm64 -arch x86_64/g' Makefile 
+gsed -i 's/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS)/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS) -arch arm64 -arch x86_64/g' source/giflib-5.2.2/Makefile 
 source "../common/make_build.sh"
