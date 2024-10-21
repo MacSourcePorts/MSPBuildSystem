975c0dc2f113279adb71e91c2ed21aac1f2f9aad
Tom Kidd <tomkidd@gmail.com>
2024-10-21 13:56:03 -0500

    Tweaks for buildserver...
    ...but like OpenJK, can't build without Rosetta

--- jk2mv/macsourceports_universal2.sh
+++ jk2mv/macsourceports_universal2.sh
@@ -13,23 +13,32 @@ source ../common/constants.sh
 
 cd ../../${PROJECT_NAME}
 
-# reset to the main branch
-echo git checkout ${GIT_DEFAULT_BRANCH}
-git checkout ${GIT_DEFAULT_BRANCH}
+export ARM64_PREFIX_PATH=/opt/Homebrew
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+	echo "Skipping git because we're on the build server"
+    export ARM64_PREFIX_PATH=/usr/local
+	export RANLIB=/usr/bin/ranlib
+else
+    # reset to the main branch
+    echo git checkout ${GIT_DEFAULT_BRANCH}
+    git checkout ${GIT_DEFAULT_BRANCH}
 
-# fetch the latest 
-echo git pull
-git pull
+    # fetch the latest 
+    echo git pull
+    git pull
 
-# for this one we have to check out latest on the mvsdk repo as well
-echo cd src/mvsdk
-cd src/mvsdk
-echo git checkout ${GIT_DEFAULT_BRANCH}
-git checkout ${GIT_DEFAULT_BRANCH}
-echo git pull
-git pull
-echo cd ../../
-cd ../../
+    # for this one we have to check out latest on the mvsdk repo as well
+    # TODO: still necessary? 
+    echo cd src/mvsdk
+    cd src/mvsdk
+    echo git checkout ${GIT_DEFAULT_BRANCH}
+    git checkout ${GIT_DEFAULT_BRANCH}
+    echo git pull
+    git pull
+    echo cd ../../
+    cd ../../
+
+fi
 
 # we just do the latest on this one as they haven't had a release tag since 2018
 
@@ -48,7 +57,8 @@ cmake \
 -DCMAKE_PREFIX_PATH=/usr/local \
 -DCMAKE_TOOLCHAIN_FILE=../../MSPBuildSystem/jk2mv/x86_64.cmake \
 ..
-make -j$NCPU
+cmake --build . --parallel -j$NCPU
+# make -j$NCPU
 cd ..
 
 rm -rf ${ARM64_BUILD_FOLDER}
@@ -60,10 +70,11 @@ cmake \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 \
 -DCMAKE_INSTALL_PREFIX=./install \
 -DCMAKE_OSX_ARCHITECTURES=arm64 \
--DCMAKE_PREFIX_PATH=/opt/Homebrew \
+-DCMAKE_PREFIX_PATH=$ARM64_PREFIX_PATH\
 -DCMAKE_TOOLCHAIN_FILE=../../MSPBuildSystem/jk2mv/arm64.cmake \
 ..
-make -j$NCPU
+cmake --build . --parallel -j$NCPU
+# make -j$NCPU
 cd ..
 
 cp ${X86_64_BUILD_FOLDER}/out/Release/jk2mvmenu_x86_64.dylib ${X86_64_BUILD_FOLDER}/out/Release/jk2mvmp.app/Contents/MacOS
@@ -72,7 +83,17 @@ mv ${X86_64_BUILD_FOLDER}/out/Release/jk2mvmp.app ${X86_64_BUILD_FOLDER}
 cp ${ARM64_BUILD_FOLDER}/out/Release/jk2mvmenu_arm64.dylib ${ARM64_BUILD_FOLDER}/out/Release/jk2mvmp.app/Contents/MacOS
 mv ${ARM64_BUILD_FOLDER}/out/Release/jk2mvmp.app ${ARM64_BUILD_FOLDER}
 
-"../MSPBuildSystem/common/build_app_bundle.sh"
+# create the app bundle
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+    "../MSPBuildSystem/common/build_app_bundle.sh" "skiplibs"
+    
+    cd ${BUILT_PRODUCTS_DIR}
+    install_name_tool -add_rpath @executable_path/. ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+    "../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+    cd ..
+else
+    "../MSPBuildSystem/common/build_app_bundle.sh"
+fi
 
 mkdir "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base"
 echo cp -a "${X86_64_BUILD_FOLDER}/out/Release/base/." "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/base"
