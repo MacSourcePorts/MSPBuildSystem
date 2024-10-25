37897b3d9c86b9a0dde6d582778412282796928d
Tom Kidd <tomkidd@gmail.com>
2024-10-24 21:30:39 -0500

    Added m4dc

---
+++ libraries/md4c.sh
@@ -0,0 +1,6 @@
+export SOURCE_URL="https://github.com/mity/md4c/archive/refs/tags/release-0.5.2.tar.gz"
+export SOURCE_FILE="md4c-release-0.5.2.tar.gz"
+export CMAKE_ARGS=""
+
+source "../common/get_source.sh"
+source "../common/cmake_build.sh"
