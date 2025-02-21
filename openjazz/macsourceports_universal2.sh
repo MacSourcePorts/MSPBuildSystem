1e6536bbcb19ba40d1bf29cb616c71e870b70b65
Tom Kidd <tomkidd@gmail.com>
2025-02-21 17:30:17 -0600

    Updated OpenJazz for newest code

--- openjazz/macsourceports_universal2.sh
+++ openjazz/macsourceports_universal2.sh
@@ -6,44 +6,76 @@ export PORT_NAME="openjazz"
 export ICONSFILENAME="openjazz"
 export EXECUTABLE_NAME="OpenJazz"
 export PKGINFO="APPLOJJ1"
-export GIT_DEFAULT_BRANCH="dev"
+export GIT_DEFAULT_BRANCH="master"
 
 #constants
 source ../common/constants.sh
 
 cd ../../${PROJECT_NAME}
 
-# reset to the main branch
-# echo git checkout ${GIT_DEFAULT_BRANCH}
-# git checkout ${GIT_DEFAULT_BRANCH}
+# If we're on the build server, assume we have the latest code
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+	echo "Skipping git because we're on the build server"
+else
+    # reset to the main branch
+    echo git checkout ${GIT_DEFAULT_BRANCH}
+    git checkout ${GIT_DEFAULT_BRANCH}
 
-# # fetch the latest 
-# echo git pull
-# git pull
+    # fetch the latest 
+    echo git pull
+    git pull
+fi
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 
 if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
     mkdir -p ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}
-    make clean
-    (ARCH="x86_64 -arch arm64" PREFIX=/usr/local/bin make -j$NCPU)
-    cp ${EXECUTABLE_NAME} ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}
-    install_name_tool -add_rpath @executable_path/. ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-    "../MSPBuildSystem/common/copy_dependencies.sh" ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-else
-    rm -rf ${X86_64_BUILD_FOLDER}
-    mkdir ${X86_64_BUILD_FOLDER}
-    mkdir -p ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
-    make clean
-    (ARCH=x86_64 PREFIX=/usr/local/bin make -j$NCPU)
-    cp ${EXECUTABLE_NAME} ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
+    cd ${BUILT_PRODUCTS_DIR}
+    cmake \
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    -DCMAKE_PREFIX_PATH=/usr/local \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DSDLMAIN_LIBRARY=/usr/local/lib/libSDL2main.a \
+    -DSDL_INCLUDE_DIR=/usr/local/include/SDL2 \
+    -DSDL_LIBRARY="/usr/local/lib/libSDL2main.a;/usr/local/lib/libSDL2.dylib;-framework Cocoa" \
+    ..
+    cmake --build . -j $NCPU
+    cp ${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+    install_name_tool -add_rpath @executable_path/. ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+    "../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
 
+    cd ..
+else
     rm -rf ${ARM64_BUILD_FOLDER}
     mkdir ${ARM64_BUILD_FOLDER}
     mkdir -p ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
-    make clean
-    (ARCH=arm64 PREFIX=/opt/Homebrew/bin make -j$NCPU)
-    cp ${EXECUTABLE_NAME} ${ARM64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
+    cd ${ARM64_BUILD_FOLDER}
+    cmake \
+    -DCMAKE_OSX_ARCHITECTURES=arm64 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    -DCMAKE_PREFIX_PATH=/opt/Homebrew \
+    -DCMAKE_INSTALL_PREFIX=/opt/Homebrew \
+    ..
+    cmake --build . -j $NCPU
+    cp ${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+
+    cd ..
+
+    rm -rf ${X86_64_BUILD_FOLDER}
+    mkdir ${X86_64_BUILD_FOLDER}
+    mkdir -p ${X86_64_BUILD_FOLDER}/${EXECUTABLE_FOLDER_PATH}
+    cd ${X86_64_BUILD_FOLDER}
+    cmake \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    -DCMAKE_PREFIX_PATH=/usr/local \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    ..
+    cmake --build . -j $NCPU
+    cp ${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+
+    cd ..
 fi
 
 # create the app bundle
