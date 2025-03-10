6ddbc90e31d6c20243879c0fedb248b45ba9ab62
Tom Kidd <tomkidd@gmail.com>
2025-03-10 16:54:33 -0500

    Added Akhenaten to build server

--- Akhenaten/macsourceports_universal2.sh
+++ Akhenaten/macsourceports_universal2.sh
@@ -4,7 +4,7 @@ export PRODUCT_NAME="Akhenaten"
 export PROJECT_NAME="Akhenaten"
 export PORT_NAME="Akhenaten"
 export ICONSFILENAME="akhenaten"
-export EXECUTABLE_NAME="Akhenaten"
+export EXECUTABLE_NAME="akhenaten"
 export GIT_TAG="v1.0"
 export GIT_DEFAULT_BRANCH="master"
 
@@ -13,38 +13,56 @@ source ../common/constants.sh
 
 cd ../../${PROJECT_NAME}
 
-# reset to the main branch
-echo git checkout ${GIT_DEFAULT_BRANCH}
-git checkout ${GIT_DEFAULT_BRANCH}
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+	echo "Skipping git because we're on the build server"
+else
+    # reset to the main branch
+    echo git checkout ${GIT_DEFAULT_BRANCH}
+    git checkout ${GIT_DEFAULT_BRANCH}
 
-# fetch the latest 
-echo git pull
-git pull
+    # fetch the latest 
+    echo git pull
+    git pull
 
-# check out the latest release tag
-# echo git checkout tags/${GIT_TAG}
-# git checkout tags/${GIT_TAG}
+	# skipping checkout since we just use the latest on this one
+fi
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 
-# create makefiles with cmake, perform builds with make
-rm -rf ${X86_64_BUILD_FOLDER}
-mkdir ${X86_64_BUILD_FOLDER}
-cd ${X86_64_BUILD_FOLDER}
-/usr/local/bin/cmake ..
-make -j$NCPU
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+    mkdir ${BUILT_PRODUCTS_DIR}
+    cd ${BUILT_PRODUCTS_DIR}
+    cmake \
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
+    ..
+    cmake --build . --parallel $NCPU
+    install_name_tool -add_rpath @executable_path/. ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+    "../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+else
+    # create makefiles with cmake, perform builds with make
+    rm -rf ${X86_64_BUILD_FOLDER}
+    mkdir ${X86_64_BUILD_FOLDER}
+    cd ${X86_64_BUILD_FOLDER}
+    /usr/local/bin/cmake ..
+    make -j$NCPU
 
-cd ..
-rm -rf ${ARM64_BUILD_FOLDER}
-mkdir ${ARM64_BUILD_FOLDER}
-cd ${ARM64_BUILD_FOLDER}
-cmake ..
-make -j$NCPU
+    cd ..
+    rm -rf ${ARM64_BUILD_FOLDER}
+    mkdir ${ARM64_BUILD_FOLDER}
+    cd ${ARM64_BUILD_FOLDER}
+    cmake ..
+    make -j$NCPU
+fi
 
 cd ..
 
 # create the app bundle
-"../MSPBuildSystem/common/build_app_bundle.sh"
+if [ "$1" == "buildserver" ] || [ "$2" == "buildserver" ]; then
+    "../MSPBuildSystem/common/build_app_bundle.sh" "skiplipo" "skiplibs"
+else
+    "../MSPBuildSystem/common/build_app_bundle.sh"
+fi
 
 #sign and notarize
 "../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
---
+++ buildbot/buildmaster/projects/Akhenaten.py
@@ -0,0 +1,51 @@
+# Akhenaten
+
+# Project where we build based the latest code because we can't update the original
+
+import os
+from buildbot.plugins import steps, util, changes, schedulers
+
+project_list = [ 
+    util.Project(name="Akhenaten",description="Akhenaten source port project")
+]
+
+change_source_list = [
+    changes.GitPoller(
+        repourl='https://github.com/dalerank/Akhenaten',
+        workdir=os.path.expanduser("~/Documents/GitHub/MacSourcePorts/MSPBuildSystem/buildbot/workdirs/Akhenaten"),
+        project="Akhenaten",
+        branches=True,
+        pollInterval=3600  # Poll every hour
+    )
+]
+
+Akhenaten_factory = util.BuildFactory()
+Akhenaten_factory.addStep(steps.Git(
+    repourl='https://github.com/dalerank/Akhenaten',
+    mode='full',  # Equivalent to 'git fetch' + 'git reset --hard'
+    method='clobber',  # Remove untracked files
+    workdir=os.path.expanduser("~/Documents/GitHub/MacSourcePorts/Akhenaten"),
+    name="Git Pull Latest Akhenaten Code",
+    haltOnFailure=True
+))
+Akhenaten_factory.addStep(steps.ShellCommand(
+    command=["/bin/bash", os.path.expanduser("~/Documents/GitHub/MacSourcePorts/MSPBuildSystem/Akhenaten/macsourceports_universal2.sh"), "notarize", "buildserver"],
+    workdir=os.path.expanduser("~/Documents/GitHub/MacSourcePorts/MSPBuildSystem/Akhenaten"),
+    name="Run Build Script",
+    haltOnFailure=True
+))
+
+builder_configs = [
+    util.BuilderConfig(name="Akhenaten-builder", workernames=["worker1"], factory=Akhenaten_factory, project="Akhenaten")
+]
+
+scheduler_list = [ 
+    schedulers.SingleBranchScheduler(
+        name="Akhenaten-changes",
+        change_filter=util.ChangeFilter(project='Akhenaten', branch='main'),
+        treeStableTimer=None,
+        builderNames=["Akhenaten-builder"]),
+    schedulers.ForceScheduler(
+        name="Akhenaten-force",
+        builderNames=["Akhenaten-builder"])
+]
