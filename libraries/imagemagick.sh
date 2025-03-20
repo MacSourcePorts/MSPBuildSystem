bdc4b137477982ff0312a0cfe4c73218789802c7
Tom Kidd <tomkidd@gmail.com>
2025-03-20 17:12:59 -0500

    Added ImageMagick
    Note that I compiled it without LQR because I couldn't get it to build with that and so far the only things using ImageMagick are just using it to make icons. If this becomes a problem later this is why.

---
+++ libraries/imagemagick.sh
@@ -0,0 +1,9 @@
+export SOURCE_URL="https://imagemagick.org/archive/releases/ImageMagick-7.1.1-44.tar.xz"
+export CONFIGURE_ARGS="--enable-osx-universal-binary=no --disable-silent-rules --disable-opencl --enable-shared --enable-static --with-freetype=yes --with-gvc=no --with-modules --with-openjp2 --with-openexr --with-webp=yes --with-heic=yes --with-raw=yes --with-gslib --with-gs-font-dir=/usr/local/share/ghostscript/fonts --without-lqr --without-djvu --without-fftw --without-pango --without-wmf --enable-openmp --without-x ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp LDFLAGS=-lomp -lz"
+export CONFIGURE_ARGS="--enable-osx-universal-binary=no --disable-silent-rules --disable-opencl --enable-shared --enable-static --with-freetype=yes --with-gvc=no --with-modules --with-openjp2 --with-openexr --with-webp=yes --with-heic=yes --with-raw=yes --with-gslib --with-gs-font-dir=/usr/local/share/ghostscript/fonts --without-lqr --without-djvu --without-fftw --without-pango --without-wmf --enable-openmp --without-x LDFLAGS=-lomp --prefix=/usr/local/"
+
+# CXX="/usr/local/bin/g++-14"
+# CC="/usr/local/bin/gcc-14"
+
+source "../common/get_source.sh"
+source "../common/make_build_lipo.sh"
