bec7c274101047ad1bd081ecedbe260cf49a3054
Tom Kidd <tomkidd@gmail.com>
2025-05-13 20:48:30 -0500

    Updated RBDOOM-3-BFG

--- RBDOOM-3-BFG/macsourceports_universal2.sh
+++ RBDOOM-3-BFG/macsourceports_universal2.sh
@@ -1,76 +1,93 @@
 # game/app specific values
-export APP_VERSION="1.5.1"
+export APP_VERSION="1.6.0"
 export PRODUCT_NAME="RBDoom3BFG"
 export PROJECT_NAME="RBDOOM-3-BFG"
 export PORT_NAME="RBDOOM-3-BFG"
 export EXECUTABLE_NAME="RBDoom3BFG"
 export ICONSFILENAME="RBDoom3BFG"
 export PKGINFO="APPLRBD3"
-export GIT_TAG="v1.5.1"
-export GIT_DEFAULT_BRANCH="master"
+export GIT_TAG="v1.6.0"
+export GIT_DEFAULT_BRANCH="rpsubsets-and-pc"
 
 #constants
 source ../common/constants.sh
 
 cd ../../${PROJECT_NAME}
 
+# because we do a patch, we need to reset any changes
+echo git reset --hard
+git reset --hard
+
 # reset to the main branch
 echo git checkout ${GIT_DEFAULT_BRANCH}
 git checkout ${GIT_DEFAULT_BRANCH}
 
-# fetch the latest 
+# # fetch the latest 
 echo git pull
 git pull
 
-# check out the latest release tag
-echo git checkout tags/${GIT_TAG}
-git checkout tags/${GIT_TAG}
+# # check out the latest release tag
+# echo git checkout tags/${GIT_TAG}
+# git checkout tags/${GIT_TAG}
+
+gsed -i '/#define[[:space:]]\+fdopen(fd,mode)[[:space:]]\+NULL/ d' neo/libs/zlib/zutil.h
+
+export VULKAN_SDK=~/VulkanSDK/1.4.313.0/macOS
+export DYLD_LIBRARY_PATH=$VULKAN_SDK/lib:$DYLD_LIBRARY_PATH
+export CMAKE_PREFIX_PATH=$VULKAN_SDK:$CMAKE_PREFIX_PATH
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 
 rm -rf ${X86_64_BUILD_FOLDER}
 mkdir ${X86_64_BUILD_FOLDER}
 cd ${X86_64_BUILD_FOLDER}
-cmake -G "Unix Makefiles" \
--DCMAKE_OSX_ARCHITECTURES=x86_64 \
--DCMAKE_C_FLAGS_RELEASE="-arch x86_64" \
--DCMAKE_PREFIX_PATH=/usr/local \
+
+cmake \
+-G "Unix Makefiles" \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 \
--DSDL2=ON \
+-DCMAKE_OSX_ARCHITECTURES=x86_64 \
+-DCMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/usr/local:$VULKAN_SDK \
+-DSDL2_DIR:PATH=/usr/local/lib/cmake/SDL2 \
 -DFFMPEG=OFF \
 -DBINKDEC=ON \
+-DUSE_MoltenVK=ON \
 -DOPENAL_LIBRARY=/usr/local/opt/openal-soft/lib/libopenal.dylib \
--DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include \
-../neo \
--Wno-dev
+-DOPENAL_INCLUDE_DIR=/usr/local/opt/openal-soft/include  \
+../neo -Wno-dev
 
 cd ..
 rm -rf ${ARM64_BUILD_FOLDER}
 mkdir ${ARM64_BUILD_FOLDER}
 cd ${ARM64_BUILD_FOLDER}
-cmake -G "Unix Makefiles" \
+
+cmake \
+-G "Unix Makefiles" \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 \
--DSDL2=ON \
+-DCMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+-DCMAKE_PREFIX_PATH=/opt/homebrew:$VULKAN_SDK \
 -DFFMPEG=OFF \
 -DBINKDEC=ON \
+-DUSE_MoltenVK=ON \
 -DOPENAL_LIBRARY=/opt/homebrew/opt/openal-soft/lib/libopenal.dylib \
--DOPENAL_INCLUDE_DIR=/opt/homebrew/opt/openal-soft/include \
-../neo \
--Wno-dev
+-DOPENAL_INCLUDE_DIR=/opt/homebrew/opt/openal-soft/include  \
+../neo -Wno-dev
 
 # perform builds with make
 cd ..
 cd ${X86_64_BUILD_FOLDER}
 make -j$NCPU
 mkdir -p ${EXECUTABLE_FOLDER_PATH}
+install_name_tool -change @rpath/libMoltenVK.dylib /usr/local/lib/libMoltenVK.dylib ${EXECUTABLE_NAME}
 mv ${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
 
 cd ..
 cd ${ARM64_BUILD_FOLDER}
 make -j$NCPU
 mkdir -p ${EXECUTABLE_FOLDER_PATH}
+install_name_tool -change @rpath/libMoltenVK.dylib /opt/homebrew/lib/libMoltenVK.dylib ${EXECUTABLE_NAME}
 mv ${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
 
 cd ..
@@ -79,12 +96,10 @@ cd ..
 "../MSPBuildSystem/common/build_app_bundle.sh"
 
 #create any app-specific directories
-if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/idlib" ]; then
-	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/idlib" || exit 1;
+if [ ! -d "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/base" ]; then
+	mkdir -p "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/base" || exit 1;
+	cp -a base/* "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/base"
 fi
-
-#lipo any app-specific things
-lipo ${X86_64_BUILD_FOLDER}/idlib/libidlib.a ${ARM64_BUILD_FOLDER}/idlib/libidlib.a -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/idlib/libidlib.a" -create
 
 #sign and notarize
 "../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
