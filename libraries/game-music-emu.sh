6982d5348bc2fc0073e1d3925541819a02d60118
Tom Kidd <tomkidd@gmail.com>
2025-07-12 16:47:51 -0500

    Fixed libgme

--- libraries/game-music-emu.sh
+++ libraries/game-music-emu.sh
@@ -1,6 +1,6 @@
 export SOURCE_URL="https://github.com/libgme/game-music-emu/archive/refs/tags/0.6.3.zip"
 export SOURCE_FILE="game-music-emu-0.6.3.zip"
-export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON -DINSTALL_MANPAGES=OFF"
+export CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON -DINSTALL_MANPAGES=OFF -DENABLE_UBSAN=OFF"
 
 source "../common/get_source.sh"
 source "../common/cmake_build.sh"
