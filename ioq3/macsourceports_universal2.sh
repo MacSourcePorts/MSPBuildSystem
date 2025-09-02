6584430233dff9e12dd5239b50c74ca0e205aa5e
Tom Kidd <tomkidd@gmail.com>
2025-09-01 19:33:47 -0500

    ioq3 moved from make to cmake

--- ioq3/macsourceports_universal2.sh
+++ ioq3/macsourceports_universal2.sh
@@ -40,13 +40,14 @@ if [ -d build ]; then
 	rm -rf build
 fi
 
-"./make-macosx-ub2.sh" release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --parallel $NCPU
 
 if [ -d "${BUILT_PRODUCTS_DIR}" ]; then
 	rm -r "${BUILT_PRODUCTS_DIR}"
 fi
 mkdir "${BUILT_PRODUCTS_DIR}"
-cp -R build/release-darwin-universal2/"${WRAPPER_NAME}" "${BUILT_PRODUCTS_DIR}"
+cp -R build/Release/"${WRAPPER_NAME}" "${BUILT_PRODUCTS_DIR}"
 
 cp "../MSPBuildSystem/ioq3/ioquake3.icns" "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 gsed -i 's|org.ioquake.ioquake3|com.macsourceports.ioquake3|' "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
