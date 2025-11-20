75f7b84109bb21d9b2730fefbf2388293c95d428
Tom Kidd <tomkidd@gmail.com>
2025-11-19 22:46:41 -0600

    Added MacWolfSDL

---
+++ MacWolfSDL/macsourceports_universal2.sh
@@ -0,0 +1,56 @@
+# game/app specific values
+export APP_VERSION="1.1.0"
+export PRODUCT_NAME="MacWolfSDL"
+export PROJECT_NAME="MacWolfSDL"
+export PORT_NAME="MacWolfSDL"
+export ICONSFILENAME="MacWolfSDL"
+export EXECUTABLE_NAME="MacWolfSDL"
+export PKGINFO="APPLBGDM"
+export GIT_DEFAULT_BRANCH="main"
+
+#constants
+source ../common/constants.sh
+export MINIMUM_SYSTEM_VERSION="10.14"
+
+cd ../../${PROJECT_NAME}
+
+if [ -n "$3" ]; then
+	export APP_VERSION="${3/v/}"
+	export GIT_TAG="$3"
+	echo "Setting version / tag to: " "$APP_VERSION" / "$GIT_TAG"
+else
+	echo "Leaving version / tag at : " "$APP_VERSION" / "$GIT_TAG"
+
+    # reset to the main branch
+    # echo git checkout ${GIT_DEFAULT_BRANCH}
+    # git checkout ${GIT_DEFAULT_BRANCH}
+
+    # # fetch the latest 
+    # echo git pull
+    # git pull
+fi
+
+rm -rf ${BUILT_PRODUCTS_DIR}
+
+mkdir ${BUILT_PRODUCTS_DIR}
+mkdir -p ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}
+mkdir -p ${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}
+cd ${BUILT_PRODUCTS_DIR}
+cmake \
+-DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+-DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 \
+..
+cmake --build . --parallel $NCPU
+# cp source/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} ${EXECUTABLE_FOLDER_PATH}
+"../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} ${FRAMEWORKS_FOLDER_PATH}
+
+cd ..
+
+# create the app bundle
+"../MSPBuildSystem/common/build_app_bundle.sh" "skiplipo" "skiplibs"
+
+# #sign and notarize
+"../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
+
+# #create dmg
+"../MSPBuildSystem/common/package_dmg.sh"
