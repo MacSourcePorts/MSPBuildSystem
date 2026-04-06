5ebaf1625ced7e060189869c0067e1a224cf2762
Tom Kidd <tomkidd@gmail.com>
2026-04-06 08:37:32 -0500

    Bump Zuma-Portable to 0.3

--- Zuma-Portable/macsourceports_universal2.sh
+++ Zuma-Portable/macsourceports_universal2.sh
@@ -1,5 +1,5 @@
 # game/app specific values
-export APP_VERSION="0.1"
+export APP_VERSION="0.3"
 export PRODUCT_NAME="Zuma-Portable"
 export PROJECT_NAME="Zuma-Portable"
 export PORT_NAME="Zuma-Portable"
