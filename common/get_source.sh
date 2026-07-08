59e0c3d10d8688891db945558cfdb23609102a82
Tom Kidd <tomkidd@gmail.com>
2026-07-08 08:57:25 -0500

    Fixes for libs

--- common/get_source.sh
+++ common/get_source.sh
@@ -5,7 +5,6 @@ cd source
 curl -JLO ${SOURCE_URL}
 
 if [ -z "${SOURCE_FILE}" ]; then
-    # SOURCE_FILE=${SOURCE_URL##*/}
     SOURCE_FILE=$(ls -t | head -n1)
 fi
 
@@ -19,11 +18,20 @@ elif [[ ${SOURCE_URL} == *.tgz ]]; then
     if [ -z "${SOURCE_FOLDER}" ]; then
         SOURCE_FOLDER=${SOURCE_FILE%.*}
     fi
+elif [[ ${SOURCE_URL} == *.7z ]]; then
+    tar -xzvf ${SOURCE_FILE}
+    if [ -z "${SOURCE_FOLDER}" ]; then
+        SOURCE_FOLDER=${SOURCE_FILE%.*}
+    fi
 else
     tar -xzvf ${SOURCE_FILE}
     if [ -z "${SOURCE_FOLDER}" ]; then
         SOURCE_FOLDER=${SOURCE_FILE%.*.*}
     fi
 fi
+
+echo SOURCE_URL: ${SOURCE_URL}
+echo SOURCE_FILE: ${SOURCE_FILE}
+echo SOURCE_FOLDER: ${SOURCE_FOLDER}
 
 cd ..
--- libraries/jsoncpp.sh
+++ libraries/jsoncpp.sh
@@ -5,8 +5,4 @@ export MESON_FLAGS=""
 
 source "../common/get_source.sh"
 
-curl -JLO https://github.com/open-source-parsers/jsoncpp/commit/3d47db0edcfa5cb5a6237c43efbe443221a32702.patch?full_index=1
-mv 3d47db0edcfa5cb5a6237c43efbe443221a32702.patch source
-patch -d source/${SOURCE_FOLDER} < source/3d47db0edcfa5cb5a6237c43efbe443221a32702.patch
-
 source "../common/meson_build.sh"
