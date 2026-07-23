8139594abf98edc4079ccb9d150beafb3ec1d988
Tom Kidd <tomkidd@gmail.com>
2026-07-22 23:55:02 -0500

    Added QSS-M

---
+++ QSS-M/macsourceports_universal2.sh
@@ -0,0 +1,42 @@
+# game/app specific values
+export APP_VERSION="1.6.5"
+export PRODUCT_NAME="QSS-M"
+export PROJECT_NAME="QSS-M"
+export PORT_NAME="QSS-M"
+export ICONSFILENAME="QSS-M"
+export EXECUTABLE_NAME="QSS-M"
+export GIT_TAG="1.6.5"
+
+# constants
+source ../common/constants.sh
+export MINIMUM_SYSTEM_VERSION="10.13"
+
+cd ../../${PROJECT_NAME}
+
+if [ -n "$3" ]; then
+	export APP_VERSION="${3/v/}"
+	export GIT_TAG="$3"
+	echo "Setting version / tag to: " "$APP_VERSION" / "$GIT_TAG"
+else
+	echo "Leaving version / tag at: " "$APP_VERSION" / "$GIT_TAG"
+fi
+
+rm -rf ${BUILT_PRODUCTS_DIR}
+mkdir ${BUILT_PRODUCTS_DIR}
+
+# This one has a script that does most of the work
+# Note that this folder gets renamed to "macOS" after 1.6.5 sometime
+
+cd MacOSX
+./build-macos.sh
+cd ..
+
+mv "MacOSX/build/Release/QSS-M.app" "${BUILT_PRODUCTS_DIR}"
+
+"../MSPBuildSystem/common/build_app_bundle.sh" "skiplipo" "skiplibs"
+
+# #sign and notarize
+"../MSPBuildSystem/common/sign_and_notarize.sh" "$1"
+
+# #create dmg
+"../MSPBuildSystem/common/package_dmg.sh" "skipcleanup"
