fd4316c335072b5d758faf19f4103d2c3e632115
Tom Kidd <tomkidd@gmail.com>
2026-09-04 16:55:59 -0500

    Update giflib.sh

--- libraries/giflib.sh
+++ libraries/giflib.sh
@@ -4,5 +4,5 @@ export SOURCE_URL=${GIFLIB_URL}
 export CONFIGURE_ARGS=""
 
 source "../common/get_source.sh"
-gsed -i 's/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS)/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS) -arch arm64 -arch x86_64/g' source/giflib-5.2.2/Makefile 
+gsed -i 's/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS)/CFLAGS  = -std=gnu99 -fPIC -Wall -Wno-format-truncation $(OFLAGS) -arch arm64 -arch x86_64/g' source/${SOURCE_FOLDER}/Makefile 
 source "../common/make_build.sh"
