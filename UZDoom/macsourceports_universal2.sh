4f40b3a07f3c7d5a44fd26a64713c0c247b45984
Tom Kidd <tomkidd@gmail.com>
2026-09-09 21:34:00 -0500

    Fix UZDoom build
    Split into arch builds, fix on non-Rosetta servers

--- UZDoom/macsourceports_universal2.sh
+++ UZDoom/macsourceports_universal2.sh
@@ -21,20 +21,22 @@ else
 	echo "Leaving version at : $APP_VERSION"
 fi
 
-gsed -i '/if( ${TARGET_ARCHITECTURE} MATCHES "x86_64" )/,/endif()/d' CMakeLists.txt
-gsed -i '/if( ${TARGET_ARCHITECTURE} MATCHES "x86_64" )/,/endif()/d' src/CMakeLists.txt
-gsed -i '/if ( ${TARGET_ARCHITECTURE} MATCHES "arm64" )/,/endif()/d' src/CMakeLists.txt
-gsed -i '/if (NOT ${TARGET_ARCHITECTURE} MATCHES "arm" )/,/endif()/d' src/CMakeLists.txt
-gsed -i '/if (MSVC AND ${TARGET_ARCHITECTURE} MATCHES "arm")/,/endif()/d' src/CMakeLists.txt
+# gsed -i '/if( ${TARGET_ARCHITECTURE} MATCHES "x86_64" )/,/endif()/d' CMakeLists.txt
+# gsed -i '/if( ${TARGET_ARCHITECTURE} MATCHES "x86_64" )/,/endif()/d' src/CMakeLists.txt
+# gsed -i '/if ( ${TARGET_ARCHITECTURE} MATCHES "arm64" )/,/endif()/d' src/CMakeLists.txt
+# gsed -i '/if (NOT ${TARGET_ARCHITECTURE} MATCHES "arm" )/,/endif()/d' src/CMakeLists.txt
+# gsed -i '/if (MSVC AND ${TARGET_ARCHITECTURE} MATCHES "arm")/,/endif()/d' src/CMakeLists.txt
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 mkdir -p ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}
 mkdir -p ${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}
-cd ${BUILT_PRODUCTS_DIR}
 
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
+cd ${ARM64_BUILD_FOLDER}
 cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -DCMAKE_OSX_ARCHITECTURES="arm64" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DBUILD_SHARED_LIBS=OFF \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
@@ -42,25 +44,57 @@ cmake \
   -DOPENAL_INCLUDE_DIR=/usr/local/include/AL \
   -DOPENAL_LIBRARY=/usr/local/lib/libopenal.dylib \
   -DVPX_INCLUDE_DIR=/usr/local/include \
-  -DVPX_LIBRARIES=/usr/local/lib/libvpx.a \
+  -DVPX_LIBRARIES=/usr/local/lib/libvpx.9.dylib \
   -DDYN_OPENAL=OFF \
+  -DDYN_SNDFILE=OFF \
   -DHAVE_VULKAN=ON \
   -DHAVE_GLES2=OFF \
   -G Ninja \
   ..
 cmake --build . --parallel $NCPU
 
-"../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} ${FRAMEWORKS_FOLDER_PATH}
+ls ImportExecutables.cmake
 
+cd ..
+
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+cd ${X86_64_BUILD_FOLDER}
+cmake \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+  -DFORCE_CROSSCOMPILE=ON \
+  -DIMPORT_EXECUTABLES=$(pwd)/../${ARM64_BUILD_FOLDER}/ImportExecutables.cmake \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  -DOPENAL_INCLUDE_DIR=/usr/local/include/AL \
+  -DOPENAL_LIBRARY=/usr/local/lib/libopenal.dylib \
+  -DVPX_INCLUDE_DIR=/usr/local/include \
+  -DVPX_LIBRARIES=/usr/local/lib/libvpx.9.dylib \
+  -DDYN_OPENAL=OFF \
+  -DDYN_SNDFILE=OFF \
+  -DHAVE_VULKAN=ON \
+  -DHAVE_GLES2=OFF \
+  -G Ninja \
+  ..
+cmake --build . --parallel $NCPU
+
+cd ..
+
+# create the app bundle
+"../MSPBuildSystem/common/build_app_bundle.sh" "skiplibs"
+
+cd ${BUILT_PRODUCTS_DIR}
+"../../MSPBuildSystem/common/copy_dependencies.sh" ${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME} ${FRAMEWORKS_FOLDER_PATH}
 cd ..
 
 gsed -i "s|org.zdoom.UZDoom|com.macsourceports.UZDoom|" "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
 gsed -i "s|Development Version|${APP_VERSION}|" "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
 gsed -i "s|zdoom.icns|UZDoom.icns|" "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
 
-echo rm "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/zdoom.icns";
 rm "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/zdoom.icns";
-echo cp "${ICONSDIR}/${ICONS}" "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/${ICONS}" || exit 1;
 cp "${ICONSDIR}/${ICONS}" "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/${ICONS}" || exit 1;
 
 #sign and notarize
